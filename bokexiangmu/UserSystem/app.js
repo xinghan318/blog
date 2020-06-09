8b3466c698fe9a906a2db5748e7fc50ac4cc7671
xinghan318 <2936541941@qq.com>
2020-06-09 21:31:16 +0800

    提示：6月9日提交

---
+++ bokexiangmu/UserSystem/app.js
@@ -0,0 +1,44 @@
+// 1.导入包
+// 1.1 导入express框架
+const express = require('express');
+// 1.2 导入path模块
+const path = require('path')
+
+// 2.设置包
+// 2.1 使用express方法创建web服务
+const app = express();
+// 2.2 设置静态资源目录
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 3.处理请求
+
+
+
+// 4.指定端口启动服务
+app.listen(80, () => {
+    console.log("服务器已启动，请打开http://localhost");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
